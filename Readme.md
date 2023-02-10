78469c18b80c8720bef06f31b5cd83c66c22c426
Katiekatt117 <123973932+Katiekatt117@users.noreply.github.com>
2023-02-10 11:23:37 -0600

    Initial Commit
    Added files included some CSS

---
+++ Readme.md
@@ -0,0 +1,1 @@
+A website on Different Types of Cherry Blossoms and How They Bloom
---
+++ script.js
@@ -0,0 +1,27 @@
+// let apple = 1;
+// let myname = '';
+
+// function applepie(){
+//     let ingredientX = 5;
+//     console.log(apple);
+// }
+
+// let pie2 = function(cinnamon) {
+//     console.log(apple + cinnamon);
+// }
+
+// function whodat() {
+//     myname = prompt("what is your name?");
+//     if (myname != 'Kat') {
+//         alert('ERROR HACKER ERROR');
+//     } else {}
+//     alert("hello, " + myname);
+// }
+
+// // applepie();
+
+// // pie2(9)
+
+// // console.log(ingredientX);
+
+// whodat();
